3b302d7f620d5340bb1d0ebb3ea67de52a46aa05
nuancesb <sophcottin@gmail.com>
2016-08-09 10:50:04 +0100

    return a coin if amount is equel to a coin

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,9 +1,11 @@
 class CoinChanger
   def self.change(amount)
     return [] if amount <= 0
+    
 
     [50,20,10,5,2].each do |coin|
-      return [coin,amount-coin] if amount - coin > 0
+      return [coin] if amount == coin
+      return [coin,amount-coin] if amount - coin > 0  
     end
     return [amount]
   end
